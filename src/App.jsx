62ef38fd90f8519ee7ed2b50931d155ef0073c3c
Mohammednur <mamebb2023@gmail.com>
2024-03-24 10:46:55 +0300

    Adding Sourse Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem]">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,9 @@
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={`${className || ""}`}>
+      Helping people create beautiful content at
+    </div>
+  );
+};
+
+export default CompanyLogos;
