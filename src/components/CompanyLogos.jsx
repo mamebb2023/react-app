837ac64805e7e4f2ba6471a8f2eeee600136988b
Mohammednur <mamebb2023@gmail.com>
2024-03-24 11:05:11 +0300

    Update: Adding Company Logs

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,7 +1,21 @@
+import { companyLogos } from "../constants";
+
 const CompanyLogos = ({ className }) => {
   return (
     <div className={`${className || ""}`}>
-      Helping people create beautiful content at
+      <h6 className="tagline mb-6 text-center text-n-50">
+        Helping people create beautiful content at
+      </h6>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+          >
+            <img src={logo} width={134} height={28} alt={logo} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
