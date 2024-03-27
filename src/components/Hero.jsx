382294786d9ba583fa36e9ba83f74f573ed115cc
Mohammednur <mamebb2023@gmail.com>
2024-03-27 05:48:35 +0300

    Update: Adding Components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
   return (
     <Section
       id="hero"
-      className="pt-[12rem] -mt-[5.25rem] "
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
